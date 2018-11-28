e94e45ef9f39b0a58d4be272933b7ca10cc2a724
Bryan Z <bryan@zis.es>
2018-11-28 15:57:56 -0600

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
             data: $(this).serializeArray(),
             dataType:"json",
             success:function(json){
-                alert(json);
 
                 window.location = redirect;
             }
